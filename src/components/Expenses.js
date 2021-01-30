0d7ebd257593f3ccb40a5dfac9a953f3bd42928f
John Schellinger <jschellinger@orchardsoft.com>
2021-01-29 23:07:26 -0500

    Convert expense and schedule tables

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import _ from 'lodash';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
 import { DataGrid } from '@material-ui/data-grid';
 import Title from './Title';
 
 const useStyles = makeStyles((theme) => {
     return {
-        table: {
-            minWidth: 100,
-        },
         dataGridContainer: {
             display: 'flex',
             height: '100%'
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,117 +4,89 @@ import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { DataGrid } from '@material-ui/data-grid';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
-
 const useStyles = makeStyles((theme) => ({
-  table: {
-    overflow: 'hidden',
-  },
-  seeMore: {
-    marginTop: theme.spacing(3),
-  },
+	dataGridContainer: {
+		display: 'flex',
+		height: '100%'
+	},
+	seeMore: {
+		marginTop: theme.spacing(3),
+	},
 }));
 
 export default function Schedule() {
-  const [paymentSchedule, setPaymentSchedule] = React.useState([]);
+	const [scheduleEvents, setScheduleEvents] = React.useState([]);
 
-  React.useEffect(() => {
-    async function fetchPaymentSchedule() {
-      await fetch('/schedule')
-        .then(res => res.json())
-        .then(data => {
-          const paymentSchedule = _.sortBy(data, row => row.rank);
-          setPaymentSchedule(paymentSchedule);
-        })
-        .catch(error => console.log(error))
-    }
+	React.useEffect(() => {
+		async function fetchScheduleEvents() {
+			await fetch('/schedule')
+				.then(res => res.json())
+				.then(data => {
+					setScheduleEvents(
+						_.sortBy(data, row => row.rank)
+							.map(scheduleEvent => {
+								scheduleEvent.id = scheduleEvent.rank; // Data-Grid requires all rows have a unique "id" property
+								delete scheduleEvent.rank;
+								return scheduleEvent;
+							}));
+				})
+				.catch(error => console.log(error))
+		}
 
-    fetchPaymentSchedule();
-  }, []);
+		fetchScheduleEvents();
+	}, []);
 
-  const formatCurrency = (key, params) => {
-    return `$${params.getValue(key).toFixed(2) || ''}`;
-  }
+	const formatCurrency = (key, params) => {
+		return `$${params.getValue(key).toFixed(2) || ''}`;
+	}
 
-  const columns = [{
-    field: 'date',
-    headerName: 'Date',
-    description: 'Date in which a debt payment should occur.',
-    flex: .30
-  }, {
-    field: 'name',
-    headerName: 'Credit Card',
-    description: 'The party to which the debt is owed.',
-    flex: .40
-  }, {
-    field: 'previousBalance',
-    headerName: 'Previous Balance',
-    description: "The debt balance prior to this date's payment amount.",
-    flex: .15,
-    valueFormatter: (params) => formatCurrency('previousBalance', params)
-  }, {
-    field: 'paymentAmount',
-    headerName: 'Amount',
-    description: 'The amount of this payment.',
-    type: 'number',
-    flex: .15,
-    valueFormatter: (params) => formatCurrency('paymentAmount', params)
-  }, {
-    field: 'nextBalance',
-    headerName: 'Next Balance',
-    description: "The debt balance after applying this date's payment amount.",
-    flex: .15,
-    valueFormatter: (params) => formatCurrency('nextBalance', params)
-  }];
+	const columns = [{
+		field: 'date',
+		headerName: 'Date',
+		description: 'Date in which a debt payment should occur.',
+		flex: .30
+	}, {
+		field: 'name',
+		headerName: 'Credit Card',
+		description: 'The party to which the debt is owed.',
+		flex: .40
+	}, {
+		field: 'previousBalance',
+		headerName: 'Previous Balance',
+		description: "The debt balance prior to this date's payment amount.",
+		type: 'number',
+		flex: .15,
+		valueFormatter: (params) => `$${params.getValue('previousBalance').toFixed(2) || ''}`
+	}, {
+		field: 'paymentAmount',
+		headerName: 'Amount',
+		description: 'The amount of this payment.',
+		type: 'number',
+		flex: .15,
+		valueFormatter: (params) => `$${params.getValue('paymentAmount').toFixed(2) || ''}`
+	}, {
+		field: 'nextBalance',
+		headerName: 'Next Balance',
+		description: "The debt balance after applying this date's payment amount.",
+		type: 'number',
+		flex: .15,
+		valueFormatter: (params) => `$${params.getValue('nextBalance').toFixed(2) || ''}`
+	}];
 
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <Title>Payment Schedule</Title>
-      <Paper className={classes.dataGridContainer}>
-        <DataGrid
-          rows={expenses}
-          columns={columns}
-          pageSize={0}
-        // checkboxSelection // don't need this yet
-        />
-      </Paper>
-      {/* <Table className='table' size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell align='right'>Before</TableCell>
-            <TableCell align='right'>Amount</TableCell>
-            <TableCell align='right'>After</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {paymentSchedule.map((row) => (
-            <TableRow key={row.rank}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell align='right'>${row.previousBalance}</TableCell>
-              <TableCell align='right'>${row.paymentAmount}</TableCell>
-              <TableCell align='right'>${row.nextBalance}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table> */}
-      <div className={classes.seeMore}>
-        <Link color='primary' href='#' onClick={preventDefault}>
-          See more orders
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<Title>Payment Schedule</Title>
+			<Paper className={classes.dataGridContainer}>
+				<DataGrid
+					rows={scheduleEvents}
+					columns={columns}
+					pageSize={0}
+				// checkboxSelection // don't need this yet
+				/>
+			</Paper>
+		</React.Fragment>
+	);
 }
