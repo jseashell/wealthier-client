b7db7e6fc26a4bbd7c325f20f656ab47fe09e4f9
John Schellinger <jschellinger@orchardsoft.com>
2021-02-03 16:58:28 -0500

    Move Paper components from Dashboard to children components

--- src/components/Debt.js
+++ src/components/Debt.js
@@ -15,74 +15,74 @@ import { makeStyles } from '@material-ui/core/styles';
 import Title from './Title';
 
 function preventDefault(event) {
-    event.preventDefault();
+	event.preventDefault();
 }
 
 const useStyles = makeStyles((theme) => {
-    return {
-        scrollPane: {
-            overflow: 'auto'
-        },
-        viewDetails: {
-            marginTop: theme.spacing(3),
-        },
-        paper: {
-            padding: theme.spacing(2),
-            display: 'flex',
-            overflow: 'hidden',
-            flexDirection: 'column',
-        }
-    };
+	return {
+		scrollPane: {
+			overflow: 'auto'
+		},
+		viewDetails: {
+			marginTop: theme.spacing(3),
+		},
+		paper: {
+			padding: theme.spacing(2),
+			display: 'flex',
+			overflow: 'hidden',
+			flexDirection: 'column',
+		}
+	};
 });
 
 export default function Debt(props) {
-    const [debts, setDebts] = React.useState([]);
+	const [debts, setDebts] = React.useState([]);
 
-    React.useEffect(() => {
-        async function fetchAllDebts() {
-            await fetch('/debt/all')
-                .then(res => res.json())
-                .then(data => setDebts(data))
-                .catch(error => console.log(error));
-        }
+	React.useEffect(() => {
+		async function fetchAllDebts() {
+			await fetch('/debt/all')
+				.then(res => res.json())
+				.then(data => setDebts(data))
+				.catch(error => console.log(error));
+		}
 
-        fetchAllDebts();
-    }, []);
+		fetchAllDebts();
+	}, []);
 
-    const classes = useStyles();
+	const classes = useStyles();
 
-    const minHeightPaper = clsx(classes.paper, props.paperHeight);
+	const minHeightPaper = clsx(classes.paper, props.paperHeight);
 
-    return (
-        <React.Fragment>
-            <Paper className={minHeightPaper}>
-                <Title>{props.title}</Title>
-                <Paper className={classes.scrollPane}>
-                    <Table className={classes.table}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Credit Card</TableCell>
-                                <TableCell>Owner</TableCell>
-                                <TableCell align='right'>Amount</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {debts.map((row) => (
-                                <TableRow key={row._id}>
-                                    <TableCell><a href={row.bankUrl}>{row.name}</a></TableCell>
-                                    <TableCell>{row.userFirstName}</TableCell>
-                                    <TableCell align='right'>${row.value.toFixed(2)}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Paper>
-                <div className={classes.viewDetails}>
-                    <Link color='primary' href='#' onClick={preventDefault}>
-                        View Details
+	return (
+		<React.Fragment>
+			<Paper className={minHeightPaper}>
+				<Title>{props.title}</Title>
+				<Paper className={classes.scrollPane}>
+					<Table className={classes.table}>
+						<TableHead>
+							<TableRow>
+								<TableCell>Credit Card</TableCell>
+								<TableCell>Owner</TableCell>
+								<TableCell align='right'>Amount</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{debts.map((row) => (
+								<TableRow key={row._id}>
+									<TableCell><a href={row.bankUrl}>{row.name}</a></TableCell>
+									<TableCell>{row.userFirstName}</TableCell>
+									<TableCell align='right'>${row.value.toFixed(2)}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</Paper>
+				<div className={classes.viewDetails}>
+					<Link color='primary' href='#' onClick={preventDefault}>
+						View Details
                 </Link>
-                </div>
-            </Paper>
-        </React.Fragment >
-    );
+				</div>
+			</Paper>
+		</React.Fragment >
+	);
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -15,56 +15,56 @@ import Schedule from './Schedule';
 import Title from './Title';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'hidden',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 340,
-        overflow: 'auto'
-    },
-    minHeight: {
-        height: 'wrap-content'
-    }
+	paper: {
+		padding: theme.spacing(2),
+		display: 'flex',
+		overflow: 'hidden',
+		flexDirection: 'column',
+	},
+	fixedHeight: {
+		height: 340,
+		overflow: 'auto'
+	},
+	minHeight: {
+		height: 'wrap-content'
+	}
 }));
 
 export default function Overview(props) {
-    const classes = useStyles();
+	const classes = useStyles();
 
-    const [showAllExpenses, setShowAllExpenses] = React.useState(false);
-    const toggleShowAllExpenses = () => {
-        setShowAllExpenses(!showAllExpenses);
-    };
+	const [showAllExpenses, setShowAllExpenses] = React.useState(false);
+	const toggleShowAllExpenses = () => {
+		setShowAllExpenses(!showAllExpenses);
+	};
 
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-    const minHeightPaper = clsx(classes.paper, classes.minHeight);
+	const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+	const minHeightPaper = clsx(classes.paper, classes.minHeight);
 
-    return (
-        <React.Fragment>
-            <Title>{props.title}</Title>
-            <Grid container spacing={3}>
-                <Grid item xs={4}>
-                    <Income paperHeight={fixedHeightPaper} />
-                </Grid>
-                <Grid item xs={5}>
-                    <Debt paperHeight={fixedHeightPaper} />
-                </Grid>
-                <Grid item xs={3}>
-                    <RemainingBalance paperHeight={fixedHeightPaper} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Expenses
-                        paperHeight={showAllExpenses ? minHeightPaper : fixedHeightPaper}
-                        toggleShowAll={toggleShowAllExpenses}
-                        showAll={showAllExpenses}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Schedule paperHeight={minHeightPaper} />
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    )
+	return (
+		<React.Fragment>
+			<Title>{props.title}</Title>
+			<Grid container spacing={3}>
+				<Grid item xs={4}>
+					<Income paperHeight={fixedHeightPaper} />
+				</Grid>
+				<Grid item xs={5}>
+					<Debt paperHeight={fixedHeightPaper} />
+				</Grid>
+				<Grid item xs={3}>
+					<RemainingBalance paperHeight={fixedHeightPaper} />
+				</Grid>
+				<Grid item xs={12}>
+					<Expenses
+						paperHeight={showAllExpenses ? minHeightPaper : fixedHeightPaper}
+						toggleShowAll={toggleShowAllExpenses}
+						showAll={showAllExpenses}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<Schedule paperHeight={minHeightPaper} />
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	)
 }
